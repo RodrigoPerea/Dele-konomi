00ba79181d881dc8738f93f544b18051ed767ffd
RodrigoPerea <chri65mb@stud.kea.dk>
2022-03-09 10:16:21 +0100

    toSting added

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,17 +1,19 @@
 package com.company;
+
 public class Item {
 
-private String catagory;
-private String item;
-
-public Item (String category, String item){
-  this.catagory=category;
-  this.item=item;
-}
+  private String catagory;
+  private String item;
 
 
+  public String toString() {
+    return catagory + " " + item;
+  }
 
-
+  public Item(String category, String item) {
+    this.catagory = category;
+    this.item = item;
+  }
 
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,12 +14,16 @@ package com.company;
                 //Create items and add to catalogue
                 Item item1 = new Item("Sport", "Skateboard");
                 catalogue.addItem(item1);
+            System.out.println(item1);
                 Item item2 = new Item("Sport", "Mountainbike");
                 catalogue.addItem(item2);
+            System.out.println(item2);
                 Item item3 = new Item("Tøj", "Smoking");
                 catalogue.addItem(item3);
+            System.out.println(item3);
                 Item item4 = new Item("Tøj", "Pailletkjole");
                 catalogue.addItem(item4);
+            System.out.println(item4);
                 ///////////////////////////////////////////////
 /*
                 // Exercise Part 2: Make program with user input instead of hardcoded values
